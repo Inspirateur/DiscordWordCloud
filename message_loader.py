e997abd2a996af0ecbb16b18012d284aee599663
Teo Orthlieb <teo.orthlieb@gmail.com>
2019-05-23 15:21:59 -0400

    tried to handle the URLs better, no idea if it works

--- message_loader.py
+++ message_loader.py
@@ -2,6 +2,7 @@ from collections import Counter
 from datetime import datetime
 from typing import Dict, List, Set
 import re
+from urllib3.util import parse_url
 from discord import Guild, Message
 import Management.ignored as ignored
 from WordCloudModel.model import Model
@@ -12,17 +13,28 @@ emoreg = re.compile(r'<a?:[^:]+:[0-9]+>')
 def add_to_model(model: Model, words: Dict[str, Counter], msg: Message, n: int = 3):
 	userid = str(msg.author.id)
 	# split the message content with the most basic tokenization
-	tokens: List = list(filter(bool, msg.content.translate(puncmap).split(' ')))
+	tokens: List = list(filter(bool, msg.content.split(' ')))
 	# separate the emojis from the words
 	wordslist: List = []
 	for token in tokens:
 		if emoreg.match(token):
+			# the token is probably an emoji
 			model.add(userid, token)
 			if token not in words:
 				words[token] = Counter()
 			words[token][userid] += 1
+		elif token.startswith("http"):
+			# the token is most likely an url
+			url = parse_url(token)
+			if len(url.host) > 0:
+				# we append the host to the wordlist
+				wordslist.append(url.host)
+			else:
+				# it wasn't an url, we treat the token as a word
+				wordslist.append(token.translate(puncmap).lower())
 		else:
-			wordslist.append(token.lower())
+			# it's probably a word (possibly with punctuation), we treat it and append it
+			wordslist.append(token.translate(puncmap).lower())
 	# add the content of the message as n-grams to echo
 	for word in wordslist:
 		model.add(userid, word)
