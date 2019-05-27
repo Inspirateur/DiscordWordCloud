91e283da73d72249e79c3d0c1292009a55ee5a9f
Teo Orthlieb <teo.orthlieb@gmail.com>
2019-05-27 14:36:40 -0400

    Live update ! (needs more testing, still naive/without decay

--- cog_cloud.py
+++ cog_cloud.py
@@ -1,18 +1,22 @@
 from asyncio.tasks import create_task
-from collections import Counter
+from collections import Counter, deque
 from datetime import datetime, timedelta
 import json
-from typing import Dict, List, Tuple, Union
+import re
+from typing import Dict, List, Set, Tuple, Union
+
 import discord.ext.commands as commands
-from discord import Activity, ActivityType, Emoji, File, Member, TextChannel
+from discord import Activity, ActivityType, Emoji, File, Guild, Member, Message, TextChannel
+
 from emoji_loader import EmojiLoader
-from message_loader import load_msgs
 import Image.make_image as make_image
+import Management.ignored as ignored
 from Model.model import Model
 try:
 	from Model.echo import Echo as ModelClass
 except ImportError:
 	from Model.baseline import Baseline as ModelClass
+globreg = re.compile(r'(<a?:[^:]+:[0-9]+>)|https?://(?:www.)?([^/\s]+)[^\s]+|(<..?[0-9]+>)|([\w-]+)|([^\s])')
 
 
 class Cloud(commands.Cog):
@@ -51,30 +55,99 @@ class Cloud(commands.Cog):
 		print(f"{ModelClass.__name__} loaded from save")
 		print("words loaded from save")
 
-	async def load_from_discord(self):
-		# for every Guild
-		for guild in self.bot.guilds:
-			# start a parallel message loader
+	def add_to_model(self, msg: Message):
+		userid = str(msg.author.id)
+		guilid = msg.guild.id
+		lasttokens = deque(maxlen=self.wordsn)
+		# build the list of tokens without the emojis
+		tokens: List[str] = []
+		# for every token in the message
+		for match in globreg.findall(msg.content):
+			# try to look for emoji in the match
+			emoji = match[0]
+			if emoji:
+				lasttokens.append(emoji)
+				# add the emoji to the model
+				self.model.add(userid, emoji)
+			else:
+				# the match is not an emoji, we look for a token
+				token = ''.join(match[1:]).lower()
+				if token:
+					lasttokens.append(token)
+					# add the token
+					tokens.append(token)
+			if len(lasttokens) == self.wordsn:
+				# we start adding n-grams to words only when lasttoken deque is full
+				for i in range(len(lasttokens)):
+					igram = " ".join([lasttokens[j] for j in range(i + 1)])
+					if igram not in self.words:
+						self.words[guilid][igram] = Counter()
+					self.words[guilid][igram][userid] += 1
+		# if the lasttokens deque is smaller than maximum, we must add the n-grams to words now
+		if len(lasttokens) < self.wordsn:
+			for i in range(len(lasttokens)):
+				igram = " ".join([lasttokens[j] for j in range(i + 1)])
+				if igram not in self.words[guilid]:
+					self.words[guilid][igram] = Counter()
+				self.words[guilid][igram][userid] += 1
+
+		# add the tokens individually to the model
+		for word in tokens:
+			self.model.add(userid, word)
+		# add the tokens as n-grams (n>=2) to the model
+		for i in range(2, self.n + 1):
+			for j in range(len(tokens) - i + 1):
+				self.model.add_n(userid, tuple(tokens[j:j + i]), 1.0 / self.n)
+
+	async def load_msgs(self, guild: Guild) -> None:
+		print(f"Start reading messages for {guild.name}")
+		if guild.id not in self.words:
 			self.words[guild.id] = {}
-			create_task(load_msgs(guild, self.model, self.n, self.words[guild.id], self.wordsn, self.limitdate, self.maxmsg))
+		# get the member object representing the bot
+		memberself = guild.me
+		# get a set of ignored channel ids for this guild
+		ignoredchans: Set[int] = set(ignored.ignore_list(guild.id))
+		# for every readable channel
+		for channel in guild.text_channels:
+			# if we can and must read the channel
+			if channel.permissions_for(memberself).read_messages and channel.id not in ignoredchans:
+				# for every message in the channel after the limit date, from new to old
+				async for message in channel.history(limit=self.maxmsg, after=self.limitdate, oldest_first=False):
+					if not message.author.bot:
+						self.add_to_model(message)
+					# also add the reactions to echo if there's any
+					for reaction in message.reactions:
+						async for user in reaction.users():
+							emoji = str(reaction.emoji)
+							user_id = str(user.id)
+							self.model.add(user_id, emoji)
+							if emoji not in self.words[guild.id]:
+								self.words[guild.id][emoji] = Counter()
+							self.words[guild.id][emoji][user_id] += 1
+		print(f"Finished reading messages for {guild.name}")
 
 	@commands.Cog.listener()
 	async def on_ready(self):
 		print(f"Logged on as {self.bot.user}!")
 		# for every Guild
 		for guild in self.bot.guilds:
-			# start a parallel emoji loader
+			# start a parallel guild emoji loader
 			emoloader = EmojiLoader(guild, make_image.emo_imgs)
 			emoloader.start()
-		self.model = ModelClass()
-		self.words = {}
-		await self.load_from_discord()
+			# start a parallel guild message loader
+			create_task(self.load_msgs(guild))
 		await self.bot.change_presence(activity=Activity(name=self.bot.command_prefix+"cloud", type=ActivityType.listening))
 
 	@commands.Cog.listener()
-	async def on_guild_join(self, guild):
-		self.words[guild.id] = {}
-		create_task(load_msgs(guild, self.model, self.words[guild.id], self.limitdate, self.maxmsg))
+	async def on_guild_join(self, guild: Guild):
+		print(f">>> Joined the guild {guild.name} !")
+		create_task(self.load_msgs(guild))
+
+	@commands.Cog.listener()
+	async def on_message(self, msg: Message):
+		# check if the author of the message is not a bot and if the channel is not ignored
+		if not msg.author.bot and msg.channel.id not in ignored.ignore_list(msg.guild.id):
+			self.add_to_model(msg)
 
 	def resolve_tag(self, ctx, w: str) -> Union[str, Emoji]:
 		if len(w) > 3:
